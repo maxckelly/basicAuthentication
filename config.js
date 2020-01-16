2f03c10dd8a714b3a12f5c9860d46094a798c263
Max Kelly <max@revelthat.com>
2020-01-16 12:24:14 +1100

    changes

---
+++ config.js
@@ -0,0 +1,5 @@
+// This key is used to add the encryption on the token. People need this key to decrypt and encrypt the token.
+// This is called 'salting'
+module.exports = {
+  secret: "this_is_a_secret_key_ssh"
+}
